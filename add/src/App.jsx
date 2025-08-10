7f57b33223394db7fa901df3581e7229e3aaec7d
Suyashnichit308 <nichitsuyash@gmail.com>
2025-08-10 18:00:41 +0530

    added from react

--- add/src/App.jsx
+++ add/src/App.jsx
@@ -6,25 +6,26 @@ import './App.css'
 function App() {
   const [num, setNum] = useState(0)
    const [num1, setNum1] = useState(0)
-   const [result,setResult]=useState( )
+   const [result,setResult]=useState()
 
    function add(){
-      return num+num1;
-   }
+      return (
+        setResult(num+num1)
+      )   }
 
   return (
     <>
       <input type="number" value={num} onChange={(event)=>{
-        setNum(event.target.value)
+        setNum(parseInt(event.target.value))
       }} />
 
         <input type="number" value={num1} onChange={(event)=>{
-         setNum1(event.target.value)
+         setNum1(parseInt(event.target.value))
       }} />
       
       <button onClick={add}>click</button>
 
-      { <h1>Result{result}</h1> }
+      { <h1>Result :{result }</h1> }
       
     </>
   )
